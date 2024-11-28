c0b0dd3f34c8c4978e46d5a7be400d02a965fb96
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-11-28 18:40:38 +0800

    update cors

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 const corsOptions = {
-    origin: [`http://localhost:3000`],
+    origin: [
+        'http://localhost:3000', 
+        'https://fitnessapi-lanuza.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
